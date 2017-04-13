a7e1584beea4319eaff32cc95451519cbfd89017
berazumis124 <berazumis@gmail.com>
2017-04-13 16:26:05 +0300

    Add disconnect event and message

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ var clientInfo = {};
 io.on('connection', function (socket){
     console.log('user connected via socket.io!');
     
+    socket.on('disconnect', function () {
+        var userData = clientInfo[socket.id];
+        if (typeof clientInfo[socket.id] !== 'undefined'){
+            socket.leave(userData.room);
+            io.to(userData.room).emit('message', {
+               name: 'Chatroom',
+                text: userData.name + ' has left!',
+                timeStamp: moment().valueOf()
+            });
+            delete clientInfo[socket.id];
+        }
+    });
+    
     socket.on('joinRoom', function (req) {
         clientInfo[socket.id] = req;
         socket.join(req.room);
